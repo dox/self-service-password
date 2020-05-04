25dfbed9f6bae38ebdd751718acfd0d4d3bd5e8d
Andrew Breakspear <andrew.breakspear@seh.ox.ac.uk>
2020-05-04 16:51:58 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,202 @@
+<?php
+ob_start();
+
+#==============================================================================
+# Includes
+#==============================================================================
+require_once("conf/config.inc.php");
+require_once("lib/vendor/defuse-crypto.phar");
+require_once("lib/functions.inc.php");
+if ($use_recaptcha) {
+    require_once("lib/vendor/autoload.php");
+}
+require_once("lib/detectbrowserlanguage.php");
+require_once("lib/vendor/PHPMailer/PHPMailerAutoload.php");
+if ($use_pwnedpasswords) {
+    require_once("lib/vendor/ron-maxweb/pwned-passwords/src/PwnedPasswords/PwnedPasswords.php");
+}
+
+#==============================================================================
+# Error reporting
+#==============================================================================
+error_reporting(0);
+if($debug) {
+    error_reporting(E_ALL);
+    // Important to get error details in case of SSL/TLS failure at connection
+    ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, 7);
+}
+
+
+#==============================================================================
+# PHP modules
+#==============================================================================
+# Init dependency check results variable
+$dependency_check_results = array();
+
+# Check PHP-LDAP presence
+if ( ! function_exists('ldap_connect') ) { $dependency_check_results[] = "nophpldap"; }
+else {
+    # Check ldap_modify_batch presence if AD mode and password change as user
+    if ( $ad_mode and $who_change_password === "user" and ! function_exists('ldap_modify_batch') ) { $dependency_check_results[] = "phpupgraderequired"; }
+}
+
+# Check PHP mhash presence if Samba mode active
+if ( $samba_mode and ! function_exists('hash') and ! function_exists('mhash') ) { $dependency_check_results[] = "nophpmhash"; }
+
+# Check PHP mbstring presence
+if ( ! function_exists('mb_internal_encoding') ) { $dependency_check_results[] = "nophpmbstring"; }
+
+# Check PHP xml presence
+if ( ! function_exists('utf8_decode') ) { $dependency_check_results[] = "nophpxml"; }
+
+# Check keyphrase setting
+if ( ( ( $use_tokens and $crypt_tokens ) or $use_sms or $crypt_answers ) and ( empty($keyphrase) or $keyphrase == "secret") ) { $dependency_check_results[] = "nokeyphrase"; }
+#==============================================================================
+# Action
+#==============================================================================
+if (!isset($default_action)) { $default_action = "change"; }
+if (isset($_GET["action"]) and $_GET["action"]) { $action = $_GET["action"]; }
+else { $action = $default_action; }
+
+# Available actions
+$available_actions = array();
+if ( $use_change ) { array_push( $available_actions, "change"); }
+if ( $change_sshkey ) { array_push( $available_actions, "changesshkey"); }
+if ( $use_questions ) { array_push( $available_actions, "resetbyquestions", "setquestions"); }
+if ( $use_tokens ) { array_push( $available_actions, "resetbytoken", "sendtoken"); }
+if ( $use_sms ) { array_push( $available_actions, "resetbytoken", "sendsms"); }
+
+# Ensure requested action is available, or fall back to default
+if ( ! in_array($action, $available_actions) ) { $action = $default_action; }
+
+# Get source for menu
+if (isset($_REQUEST["source"]) and $_REQUEST["source"]) { $source = $_REQUEST["source"]; }
+else { $source="unknown"; }
+
+#==============================================================================
+# Other default values
+#==============================================================================
+if (!isset($ldap_login_attribute)) { $ldap_login_attribute = "uid"; }
+if (!isset($ldap_fullname_attribute)) { $ldap_fullname_attribute = "cn"; }
+if (!isset($pwd_forbidden_chars)) { $pwd_forbidden_chars = ""; }
+if (!isset($hash_options)) { $hash_options = array(); }
+if (!isset($samba_options)) { $samba_options = array(); }
+if (!isset($ldap_starttls)) { $ldap_starttls = false; }
+
+# Password policy array
+$pwd_policy_config = array(
+    "pwd_show_policy"         => $pwd_show_policy,
+    "pwd_min_length"          => $pwd_min_length,
+    "pwd_max_length"          => $pwd_max_length,
+    "pwd_min_lower"           => $pwd_min_lower,
+    "pwd_min_upper"           => $pwd_min_upper,
+    "pwd_min_digit"           => $pwd_min_digit,
+    "pwd_min_special"         => $pwd_min_special,
+    "pwd_special_chars"       => $pwd_special_chars,
+    "pwd_forbidden_chars"     => $pwd_forbidden_chars,
+    "pwd_no_reuse"            => $pwd_no_reuse,
+    "pwd_diff_login"          => $pwd_diff_login,
+    "pwd_complexity"          => $pwd_complexity,
+    "use_pwnedpasswords"      => $use_pwnedpasswords,
+    "pwd_no_special_at_ends"  => $pwd_no_special_at_ends
+);
+
+if (!isset($pwd_show_policy_pos)) { $pwd_show_policy_pos = "above"; }
+if (!isset($obscure_failure_messages)) { $obscure_failure_messages = array(); }
+
+#==============================================================================
+# Email Config
+#==============================================================================
+$mailer = new PHPMailer;
+$mailer->Priority      = $mail_priority;
+$mailer->CharSet       = $mail_charset;
+$mailer->ContentType   = $mail_contenttype;
+$mailer->WordWrap      = $mail_wordwrap;
+$mailer->Sendmail      = $mail_sendmailpath;
+$mailer->Mailer        = $mail_protocol;
+$mailer->SMTPDebug     = $mail_smtp_debug;
+$mailer->Debugoutput   = $mail_debug_format;
+$mailer->Host          = $mail_smtp_host;
+$mailer->Port          = $mail_smtp_port;
+$mailer->SMTPSecure    = $mail_smtp_secure;
+$mailer->SMTPAutoTLS   = $mail_smtp_autotls;
+$mailer->SMTPAuth      = $mail_smtp_auth;
+$mailer->Username      = $mail_smtp_user;
+$mailer->Password      = $mail_smtp_pass;
+$mailer->SMTPKeepAlive = $mail_smtp_keepalive;
+$mailer->SMTPOptions   = $mail_smtp_options;
+$mailer->Timeout       = $mail_smtp_timeout;
+$mailer->LE            = $mail_newline;
+
+#==============================================================================
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Jekyll v3.8.6">
+	<title>Signin Template · Bootstrap</title>
+	
+	<link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/sign-in/">
+	
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	
+	<!-- Favicons
+<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
+	-->
+	
+	<style>
+		.bd-placeholder-img {
+			font-size: 1.125rem;
+			text-anchor: middle;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+			user-select: none;
+		}
+		
+		@media (min-width: 768px) {
+			.bd-placeholder-img-lg {
+				font-size: 3.5rem;
+			}
+		}
+	</style>
+	
+	<!-- Custom styles for this template -->
+	<link href="css/signin.css" rel="stylesheet">
+</head>
+
+
+<body class="text-center">
+
+	<form class="form-signin" action="#" method="post">
+		<img class="mb-4" src="images/logo.png" alt="" width="72" height="72">
+		<?php
+		
+		if (isset($_GET['action'])) {
+			$node = "nodes/" . $_GET['action'] . ".php";
+		} else {
+			$node = "nodes/sendtoken.php";
+		}
+		
+		include_once($node);
+		?>
+		
+		<p class="mt-5 mb-3 text-muted">&copy; 2020 www.seh.ox.ac.uk</p>
+	</form>
+</body>
+</html>
+
